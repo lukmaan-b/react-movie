4751a9243cdb28b2dfdbee875d7ae0ab7eeb64d9
lukmaan-b <lukmaan.butt@gmail.com>
2020-10-31 20:48:15 +0000

    Add SingleMoviePage & movieList component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import { Config } from './api';
 import useFetch from './useFetch';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import SingleMovie from './components/SingleMovie';
+import MovieList from './components/MovieList';
+import './App.css';
 function App() {
   const moveList = useFetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${Config.API_KEY}`
   );
-  console.log(moveList);
   return (
-    <div className="App">
-      <h1>d</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <main>
+            <h1>Movies</h1>
+            <section className="grid">
+              <MovieList movies={moveList} />
+            </section>
+          </main>
+        </Route>
+        <Route path="/movies/:id">
+          <SingleMovie movies={moveList} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
